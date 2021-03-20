d0124fa06e6c9c3a3ca6f44e2059e6c5086bbe6d
moetaz.brayek <moetaz.brayek@esprit.tn>
2021-03-20 13:24:22 +0100

    Fix Env file and mongo imports

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+import { connect } from "mongoose";
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
@@ -11,4 +11,4 @@ const connectDB = async () => {
     );
 };
 
-module.exports = connectDB;
+export default connectDB;
