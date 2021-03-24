ba50c52c44b3d99e3bb5a5cfa07129767017a5ea
moetaz.brayek <moetaz.brayek@esprit.tn>
2021-03-24 23:51:12 +0100

    lazy loading +improve import

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,4 +1,4 @@
-import React , { Suspense, lazy } from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
@@ -9,11 +9,11 @@ import LoadingGif from '@Assets/Fidget-spinner.gif'
 const Home = lazy(() => import('@FrontOfficePages/Home/Home'));
 const AdminRoute = lazy(() => import('@Routes/AdminRoute'));
 
-
+/** Moetaz Brayek Imports */
 const Login = lazy(() => import('@FrontOfficePages/LoginRegister/Login'));
-const Logout =  lazy(() => import('@FrontOfficePages/LoginRegister/Logout'));
+const Logout = lazy(() => import('@FrontOfficePages/LoginRegister/Logout'));
 const Register = lazy(() => import('@FrontOfficePages/LoginRegister/Register'));
-const ForgotPassword =  lazy(() => import('@FrontOfficePages/LoginRegister/ForgotPassword'));
+const ForgotPassword = lazy(() => import('@FrontOfficePages/LoginRegister/ForgotPassword'));
 const ResetPassword = lazy(() => import('@FrontOfficePages/LoginRegister/ResetPassword'));
 const EmailVerification = lazy(() => import('@FrontOfficePages/LoginRegister/EmailVerification'));
 // * Chihab's imports
@@ -25,20 +25,20 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-        <Suspense fallback={<img src={LoadingGif} alt="loading..." />}>
-          <Switch>
-            // * Chihab's routes
+          <Suspense fallback={<img src={LoadingGif} alt="loading..." />}>
+            <Switch>
+              // * Chihab's routes
             <Route exact={true} path="/brandSignup" component={BrandSignup} />
-            <AdminRoute exact={true} path="/admin/brandList" component={AdminBrandsList}/>
+              <AdminRoute exact={true} path="/admin/brandList" component={AdminBrandsList} />
             // * End Chihab's routes
             <Route exact={true} path="/" component={Home} />
-            <Route exact={true} path="/login" component={Login} />
-            <Route exact={true} path="/forgotPasssword" component={ForgotPassword}/>
-            <Route exact={true} path="/resetPassword" component={ResetPassword}/>
-            <Route exact={true} path="/register" component={Register} />
-            <Route exact={true} path="/EmailVerification" component={EmailVerification}/>
-            <Route exact={true} path="/logout" component={Logout} />
-          </Switch>
+              <Route exact={true} path="/login" component={Login} />
+              <Route exact={true} path="/forgotPasssword" component={ForgotPassword} />
+              <Route exact={true} path="/resetPassword" component={ResetPassword} />
+              <Route exact={true} path="/register" component={Register} />
+              <Route exact={true} path="/EmailVerification" component={EmailVerification} />
+              <Route exact={true} path="/logout" component={Logout} />
+            </Switch>
           </Suspense>
         </Container>
       </main>
